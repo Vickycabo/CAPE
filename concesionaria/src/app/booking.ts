21462c553b4d5e65c4c39eeaad499593b7782f71
Regina Perez <reginamaperez@gmail.com>
2025-11-10 21:47:32 -0300

    Cambios para autos con mas fotos  y correccion de errores de tipo (vehicle-details/vehicle-forms)

--- concesionaria/src/app/booking.ts
+++
@@ -1,8 +0,0 @@
-export interface Booking {
-    id?: string | number,
-    name: string,
-    email: string,
-    phone: string,
-    vehicleId: number
-    date: Date
-}
--- concesionaria/src/app/inquiry.ts
+++
@@ -1,8 +0,0 @@
-export interface Inquiry { //Consultas
-    id?: string | number,
-    name: string,
-    email: string,
-    phone: string,
-    message: string //Campo de consultas
-    vehicleId: number
-}
